f0da92c6eb036be555c7906b8c6ae40ed114f521
Abdul1rhman <pain008900@gmail.com>
2023-04-20 07:22:19 +0300

    fix some bugs

--- components/Nav.js
+++ components/Nav.js
@@ -1,73 +1,120 @@
-import React, { useState } from 'react';
+// import React, { useState,useRef } from 'react';
+// import { Transition } from 'react-transition-group';
+
+// const duration = 100;
+
+// const defaultStyle = {
+//   transition: `opacity ${duration}ms ease-in-out`,
+//   opacity: 0,
+// };
+
+// const transitionStyles = {
+//   entering: { opacity: 0 },
+//   entered: { opacity: 1 },
+// };
+
+// function BurgerMenu() {
+//   const [isOpen, setIsOpen] = useState(false);
+//   const nodeRef = useRef(null)
+
+//   const handleClick = () => {
+//     setIsOpen(!isOpen);
+//     document.activeElement.blur()
+//   };
+
+//   return (
+//     <nav className="burger-menu" lang='ar' dir='rtl'>
+    
+//       <button
+//         onClick={handleClick}
+//         className={`burger-menu__button ${isOpen ? 'active' : ''}`}
+//         tabIndex='-1'
+        
+//       >
+//         <span className="burger-menu__icon">
+//           <span className="burger-menu__line" />
+//           <span className="burger-menu__line" />
+//           <span className="burger-menu__line" />
+//         </span>
+// </button>
+//         <Transition in={isOpen} timeout={duration} nodeRef={nodeRef}>
+//         {(state) => (
+         
+
+          
+//             <div
+//                 className="burger-menu__items"
+//                 style={{
+//                 ...defaultStyle,
+//                 ...transitionStyles[state],
+//                 }}
+//             >
+//                 <a href="#a" className="burger-menu__item">
+//                 الرئيسية
+//                 </a>
+//                 <a href="#b" className="burger-menu__item">
+//                 استخداماته
+//                 </a>
+//                 <a href="#c" className="burger-menu__item">
+//                 ادوات
+//                 </a>
+//             </div>
+          
+//         )}
+//       </Transition>
+      
+
+//       <h1>المتأصلة </h1>
+
+      
+//     </nav>
+//   );
+// }
+
+// export default BurgerMenu;
+
+
+import React, { useState, useRef } from 'react';
 import { Transition } from 'react-transition-group';
 
-const duration = 100;
 
-const defaultStyle = {
-  transition: `opacity ${duration}ms ease-in-out`,
-  opacity: 0,
-};
-
-const transitionStyles = {
-  entering: { opacity: 0 },
-  entered: { opacity: 1 },
-};
 
 function BurgerMenu() {
   const [isOpen, setIsOpen] = useState(false);
+  const nodeRef = useRef(null);
 
   const handleClick = () => {
     setIsOpen(!isOpen);
     document.activeElement.blur()
+    
   };
 
   return (
-    <nav className="burger-menu" lang='ar' dir='rtl'>
-    
-      <button
-        onClick={handleClick}
-        className={`burger-menu__button ${isOpen ? 'active' : ''}`}
-        tabIndex='-1'
-        
-      >
-        <span className="burger-menu__icon">
-          <span className="burger-menu__line" />
-          <span className="burger-menu__line" />
-          <span className="burger-menu__line" />
+    <nav className="burger-menu" lang="ar" dir="rtl">
+      <button className={`btn-nav ${isOpen ? 'act':''}`}onClick={handleClick}>
+        <span className='logo'>
+          <span className='line'></span>
+          <span className='line'></span>
+          <span className='line'></span>
         </span>
-
-        <Transition in={isOpen} timeout={duration}>
-        {(state) => (
-         
-
-          
-            <div
-                className="burger-menu__items"
-                style={{
-                ...defaultStyle,
-                ...transitionStyles[state],
-                }}
-            >
-                <a href="#" className="burger-menu__item">
-                الرئيسية
-                </a>
-                <a href="#" className="burger-menu__item">
-                الرئيسية
-                </a>
-                <a href="#" className="burger-menu__item">
-                الرئيسية
-                </a>
-            </div>
-          
-        )}
-      </Transition>
+        
       </button>
-
-      <h1>المتأصلة </h1>
+      {isOpen && <div className='nav-it'>
+        <a href="#a">نبذة</a>
+        <a href="#b">استخدامات</a>
+        <a href="#c">ادوات</a>
+      </div>}
 
       
+      
+      <h1>
+        
+          <a>المتأصلة</a>
+        
+      </h1>
     </nav>
   );
 }
 
 export default BurgerMenu;
+
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,24 @@
 import Image from 'next/image'
 import Accordion from '@/components/Accordion'
+import { faArrowUpRightFromSquare} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 function Index() {
   return (
     <div lang='ar' dir='rtl' className='main-cont'>
    
-    <div className='image'>
+    <div className='image' >
       <Image src='/images/robot-handshake-human-background-futuristic-digital-age-PhotoRoom.png-PhotoRoom.png' width={500} height={300} alt="" className='image-comp'/>
     </div>
-    <div className='head main'>
+    <div className='head main' 
+    id='a'>  
     <h2>الذكاء الاصطناعي</h2>
       <p>تعد ثورة الذكاء الاصطناعي من أهم الثورات التكنولوجية التي شهدها العالم في العقود الأخيرة، وتمثل تغييرًا جذريًا في طريقة تفكيرنا وتعاملنا مع التكنولوجيا والحياة بشكل عام.</p>
-<a href="#s"><button className='prm-btn'>التعرف على المزيد</button></a>
+      <a href="#c"><button className='prm-btn'> التعرف على المزيد <FontAwesomeIcon icon={faArrowUpRightFromSquare} /></button></a>
     </div>
 
     
 
-    <div className='head second' id='s'>
+    <div className='head second' id='b'>
       <h2>استخداماته</h2>
       <div className='accor'>
         <Accordion
@@ -53,18 +56,29 @@ function Index() {
 
     </div>
 
-    <div className='head'>
-      <div className='caty'>
-        <h2>ادوات تستعمل الذكاء الاصطناعي</h2>
+    <div className='head' id='c'>
+    <h2>ادوات تستعمل الذكاء الاصطناعي</h2>
+      <div className='caty' >
+        
         <h3>ChatGPT</h3>
+        <div >
+        <Image src='/images/chatgpt.png' width={250} height={150} alt="" className='img-caty'/>
+
+        </div>
         <p>تم تطوير ChatGPT باستخدام تقنية التعلم العميق الحديثة المعروفة باسم GPT-3.5 وهو نموذج لغوي كبير يتم تدريبه على مجموعة واسعة من النصوص والبيانات اللغوية. يمكن استخدام ChatGPT لإجراء محادثات ذكية ومتقدمة مع المستخدمين عبر الإنترنت، حيث يستطيع النموذج فهم اللغة الطبيعية والرد على الأسئلة وتقديم المعلومات بشكل سلس وسريع. ويمكن استخدام ChatGPT لأغراض مختلفة مثل الدعم الفني، التفاعل الاجتماعي، المساعدة في المهام اليومية، والمزيد.</p>
+        <a href="https://openai.com/blog/chatgpt" target='_blank'>قم بتجربته </a>
+
       </div>
       
       
       <div className='caty'>
         <h3>Poe</h3>
+        <div >
+        <Image src='/images/poe.png' width={150} height={150} alt="" className='img-caty'/>
+
+        </div>
         <p>تطبيق Poe هو تطبيق تحليل النصوص الذي يستخدم تقنيات التعلم العميق والذكاء الاصطناعي لتحويل النصوص إلى بيانات قابلة للتشغيل. يستخدم التطبيق تحليل اللغوي النصي لفهم النصوص والكشف عن المعاني المخفية والعلاقات بينها. ويستخدم تقنيات التعلم العميق لتحسين دقة التحليل وتوفير نتائج سريعة ودقيقة.</p>
-        <p></p>
+        <a href="https://apps.apple.com/us/app/poe-fast-ai-chat/id1640745955" target='_blank'>قم بتجربته </a>
       </div>
       
       
